44e25728a695ff604741c5a0619feb4d9f81b5a3
aygharib <12767433+aygharib@users.noreply.github.com>
2024-05-25 10:11:17 -0400

    add helper scripts to copy to and from system config

---
+++ helper/1-copy-nvim-to-system-config.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Absolute path to the folder containing the script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
+
+# Go to script directory
+cd $SCRIPT_DIR
+
+cd ..
+
+# Copy nvim folder to config folder on system
+cp -r nvim ~/.config/nvim
