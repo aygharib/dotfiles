81f4ca769d5f4cd3a20ed4c96b9773e815ec3f30
Ahmad <ahmad.gharib@amd.com>
2025-03-26 12:27:05 -0400

    update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -27,5 +27,56 @@ return {
                 -- }
             })
         end
+    },
+    {
+        "folke/tokyonight.nvim", -- Add the theme plugin
+        -- "catppuccin/nvim",
+        -- 'kepano/flexoki-neovim',
+        -- name = "flexoki",
+        config = function()
+            vim.cmd("colorscheme tokyonight") -- Apply the theme
+            -- vim.cmd("colorscheme catppuccin") -- Apply the theme
+            -- vim.cmd("colorscheme flexoki") -- Apply the theme
+            -- vim.cmd('colorscheme flexoki-dark')
+        end
     }
 }
+
+-- return {}
+
+-- return {
+--     {
+--         "nvim-treesitter/nvim-treesitter",
+--         build = ":TSUpdate",
+--         config = function()
+--             local config = require("nvim-treesitter.configs")
+--             config.setup({
+--                 ensure_installed = { "c", "cpp" }, -- install parsers for C, C++
+--                 highlight = { enable = true },    -- enable syntax highlighting
+--                 indent = { enable = true },       -- enable better indentation
+--             })
+--         end
+--     },
+--     {
+--         "folke/tokyonight.nvim", -- Tokyonight theme plugin
+--     },
+--     {
+--         "catppuccin/nvim", name = "catppuccin" -- Catppuccin theme plugin
+--     },
+--     {
+--         -- Keybinding configuration for theme switching
+--         config = function()
+--             vim.keymap.set("n", "<leader>st", function()
+--                 local current_theme = vim.g.colors_name
+--                 if current_theme == "tokyonight" then
+--                     vim.cmd("colorscheme catppuccin")
+--                 else
+--                     vim.cmd("colorscheme tokyonight")
+--                 end
+--             end, { desc = "Switch theme" })
+            
+--             -- Set a default theme on startup
+--             vim.cmd("colorscheme tokyonight")
+--         end
+--     }
+-- }
